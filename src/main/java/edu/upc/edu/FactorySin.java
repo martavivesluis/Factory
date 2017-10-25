3e0ab958b85dd7ecd0dd765cded07220b97e5d5e
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2017-10-25 12:49:58 +0200

    ++ factory acabat

--- src/main/java/edu/upc/edu/FactorySin.java
+++ src/main/java/edu/upc/edu/FactorySin.java
@@ -8,6 +8,7 @@ public class FactorySin {
 
     private static FactorySin instance = null;//inicialment buit
     HashMap<String, Command> cache;
+
     private FactorySin(){
         cache = new HashMap<String, Command>();//crea una nova instancia
                     } // constructor
@@ -18,19 +19,24 @@ public class FactorySin {
              instance = new FactorySin();
          }
      return instance;}
-    public Command getCommand(String pCommand){
-       Command cmd = cache.get(pCommand);
-       if (cmd == null) {
-           cmd = loadClass(pCommand);
+
+     public Command getCommand(String pCommand){
+        log.info(pCommand)
+        Command cmd = cache.get(pCommand);
+        if (cmd == null) {
+           log.info("carregador de claesses");
+           cmd = loadClass(pCommand);//aqui dins tenim un new
            cache.put(pCommand, cmd);
-       }
+        }
+       else log.info("command de cache")
        return cmd;}
+
     private Command loadClass(String pCommand){
         if ( pCommand == "C1")
         {
             return new C1();
         }
-        if ( pCommand == "C2")
+        if ( pCommand.equals("C2"))
         {return new C2();}
         if ( pCommand == "C3")
         {return new C3();}
